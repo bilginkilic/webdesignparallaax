15ae1e06a43b6bd1f1ac8aec6d654da65c07d960
bilginkilic <blgnklc@gmail.com>
2024-06-30 13:12:22 +0100

    edinburgh

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    swcMinify: true, // Enable SWC minification
+    compiler: {
+      styledComponents: true, // Example: enable styled-components with SWC
+    }
+  };
+  
+  export default nextConfig;
